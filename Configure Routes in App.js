6aee83b9ea4ac10a010d5f670a8f9b3594a99f1e
SmitiRout <139580699+SmitiRout@users.noreply.github.com>
2025-07-24 19:07:53 +0530

    Create Configure Routes in App.js

---
+++ Configure Routes in App.js
@@ -0,0 +1,24 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Settings from './pages/Settings';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/settings" element={<Settings />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
